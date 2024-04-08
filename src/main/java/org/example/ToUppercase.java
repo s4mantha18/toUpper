9dccc06403d406e1201952111183c92b2b1ba446
s4mantha18 <samanthaoloruntoba6@gmail.com>
2024-04-08 15:08:42 +0100

    next

--- src/main/java/org/example/ToUppercase.java
+++ src/main/java/org/example/ToUppercase.java
@@ -7,8 +7,8 @@ public class ToUppercase {
     public static void main(String[] args) {
         System.out.println("Enter a word:");
         String word = new Scanner(System.in).nextLine();
-        String firstletter = word.substring(0,1);
-        String remaining = word.substring(1,word.length());
+        String firstletter = String.valueOf(word.charAt(0));
+        String remaining = word.substring(1,word.length()).toLowerCase();
         System.out.println(firstletter.toUpperCase()+remaining);
     }
 }
