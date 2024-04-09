6fdf034c5f77e1a60553f9d922c01d6b2693a905
s4mantha18 <samanthaoloruntoba6@gmail.com>
2024-04-09 22:41:44 +0100

    4th

--- src/main/java/org/example/Factorial.java
+++ src/main/java/org/example/Factorial.java
@@ -1,7 +1,4 @@
-package org.example;
-
-public class Factorial {
-    import java.util.Scanner;
+import java.util.Scanner;
 
     public class FactorialCalculator {
         public static void main(String[] args) {
