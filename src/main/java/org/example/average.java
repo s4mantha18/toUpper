09ab7cf0bef23d662f22804a002b450402896100
s4mantha18 <samanthaoloruntoba6@gmail.com>
2024-04-10 23:24:26 +0100

    average3

--- src/main/java/org/example/average.java
+++ src/main/java/org/example/average.java
@@ -3,7 +3,7 @@ package org.example;
 public class average {
     public static void main(String[] args) {
         for (int i=0; i<=10; i++){
-            int sum = i+i;
+            int sum = i++;
             System.out.println(sum);
         }
     }
