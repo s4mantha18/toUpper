62b6f4f14e163ce33bda774adda6ce0464a0df90
s4mantha18 <samanthaoloruntoba6@gmail.com>
2024-04-10 23:50:32 +0100

    reversestring3

--- src/main/java/org/example/reversestring.java
+++ src/main/java/org/example/reversestring.java
@@ -1,9 +1,12 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class reversestring {
     public static void main(String[] args) {
         String word  = "name";
         String[] wordarray = word.split("", word.length());
         System.out.println(wordarray);
+        Arrays.binarySearch(wordarray);
     }
 }
