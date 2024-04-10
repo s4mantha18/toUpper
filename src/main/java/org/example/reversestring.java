5e5b395889c580ce923b346c87d6ee30633e073c
s4mantha18 <samanthaoloruntoba6@gmail.com>
2024-04-10 23:43:04 +0100

    reversestring2

--- src/main/java/org/example/reversestring.java
+++ src/main/java/org/example/reversestring.java
@@ -3,6 +3,7 @@ package org.example;
 public class reversestring {
     public static void main(String[] args) {
         String word  = "name";
-
+        String[] wordarray = word.split("", word.length());
+        System.out.println(wordarray);
     }
 }
