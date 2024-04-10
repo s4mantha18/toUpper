2e872e6675f0be252570c92f8b149fea2a8dbf3b
s4mantha18 <samanthaoloruntoba6@gmail.com>
2024-04-10 23:52:45 +0100

    reversestring4

--- src/main/java/org/example/reversestring.java
+++ src/main/java/org/example/reversestring.java
@@ -1,12 +1,34 @@
-package org.example;
+import java.util.Scanner;
 
-import java.util.Arrays;
-
-public class reversestring {
+public class StringReversal {
     public static void main(String[] args) {
-        String word  = "name";
-        String[] wordarray = word.split("", word.length());
-        System.out.println(wordarray);
-        Arrays.binarySearch(wordarray);
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+
+        String reversedString = reverseString(input);
+
+        System.out.println("Reversed string: " + reversedString);
+
+        scanner.close();
+    }
+
+    public static String reverseString(String str) {
+        char[] charArray = str.toCharArray();
+        int left = 0;
+        int right = charArray.length - 1;
+
+        while (left < right) {
+            // Swap characters at left and right pointers
+            char temp = charArray[left];
+            charArray[left] = charArray[right];
+            charArray[right] = temp;
+
+            // Move the pointers towards the center
+            left++;
+            right--;
+        }
+
+        return new String(charArray);
     }
 }
