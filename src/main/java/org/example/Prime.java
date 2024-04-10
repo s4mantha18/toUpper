cf8a50631eba049b990ae89ae13171508bf53685
s4mantha18 <samanthaoloruntoba6@gmail.com>
2024-04-10 21:39:35 +0100

    prime3

--- src/main/java/org/example/Prime.java
+++ src/main/java/org/example/Prime.java
@@ -1,7 +1,10 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Prime{
     public static void main(String[] args) {
         System.out.println("Enter a number:");
+        int num = new Scanner(System.in).nextInt();
     }
 }
