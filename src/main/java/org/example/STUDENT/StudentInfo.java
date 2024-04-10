624a881607a7bb83eaf2f6ca6f392bad3be7086b
s4mantha18 <samanthaoloruntoba6@gmail.com>
2024-04-10 22:25:51 +0100

    studentclass2

--- src/main/java/org/example/STUDENT/StudentInfo.java
+++ src/main/java/org/example/STUDENT/StudentInfo.java
@@ -5,4 +5,27 @@ public class StudentInfo {
     private int age;
     private String grade;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
 }
