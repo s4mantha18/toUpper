f7ffcf54e271c6f9e978cf06290866efc7c01ce9
s4mantha18 <samanthaoloruntoba6@gmail.com>
2024-04-10 23:16:44 +0100

    average

---
+++ src/main/java/org/example/average.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class average {
+    public static void main(String[] args) {
+        for (int i=0; i<10; i++){
+            int sum = i;
+            System.out.println(sum);
+        }
+    }
+}
