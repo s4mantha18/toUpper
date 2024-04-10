49d9a734345c5ba92490a0156efc096e87301616
s4mantha18 <samanthaoloruntoba6@gmail.com>
2024-04-10 23:53:30 +0100

    reversestring5

--- src/main/java/org/example/reversestring.java
+++ src/main/java/org/example/reversestring.java
@@ -1,6 +1,8 @@
+package org.example;
+
 import java.util.Scanner;
 
-public class StringReversal {
+public class reversestring {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a string: ");
