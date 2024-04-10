c9506231ef5d2af9e21de39ebed33665075e265d
s4mantha18 <samanthaoloruntoba6@gmail.com>
2024-04-10 21:49:02 +0100

    prime4

--- src/main/java/org/example/Prime.java
+++ src/main/java/org/example/Prime.java
@@ -6,5 +6,10 @@ public class Prime{
     public static void main(String[] args) {
         System.out.println("Enter a number:");
         int num = new Scanner(System.in).nextInt();
+        if (num%2==0){
+            System.out.println("This is not a prime number");
+        } else{
+            System.out.println("PRIME NUMBER!!");
+        }
     }
 }
