5595440f962f8ea09e5436fc8e7ab50a79fe58eb
s4mantha18 <samanthaoloruntoba6@gmail.com>
2024-04-10 22:41:41 +0100

    studentclass3

---
+++ src/main/java/org/example/STUDENT/main.java
@@ -0,0 +1,11 @@
+package org.example.STUDENT;
+
+public class main {
+
+    public static void main(String[] args) {
+        StudentInfo student1 = new StudentInfo();
+        student1.setAge(15);
+        student1.setName("Abraham");
+        student1.setGrade("ss1");
+    }
+}
